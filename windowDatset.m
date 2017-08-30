e12e45d7c35b7ef5dde8f628565cfa4b27f46210
Alexander Gomez Villa <alexander.gomezvilla@supsi.ch>
2017-08-30 18:31:33 +0200

    Sequence separation algorithm

--- windowDatset.m
+++ windowDatset.m
@@ -7,76 +7,132 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Paths
-path = '/home/lex/MEGAsync/1_MSE/2_Projects/Project_2/2_Datasets/1_IEEE_Signal_Processing_cup_2015/';
+path = '/home/lex/Desktop/PPG_DATASET/1_IEEE_Signal_Processing_cup_2015/';
 
 
 %% Parameters
 windowSize = 5; % In seconds
-stride = 0.5; % percentage of overlaping
 Fs = 50;
+stride = 0.5; % percentage of overlaping
+step = floor(windowSize*Fs*stride);
+
+winLen = windowSize*Fs;
 
 
-WindowSig = {};
+WindowSigTrain = {};
+WindowSigTest = {};
 
+
+%% Train Set
 
 files = dir([path,'Training_data','/*.mat']);
-
-downsampleRate = 50;
-[P,Q] = rat(downsampleRate/Fs);
-
-
+contSigns = 1;
+segmentsNum = 0;
 for i=1:length(files)
     
    file = files(i).name;
    if(file(1,end-4)~='e')
        load([path,'Training_data','/',file])
-       
-       
-       part
-       newSig = resample(oldSig(1,:),P,Q);
-       sig = zeros(6,size(newSig,2));
-       newSig2 = resample(oldSig(2,:),P,Q);
-       newSig3 = resample(oldSig(3,:),P,Q);
-       newSig4 = resample(oldSig(4,:),P,Q);
-       newSig5 = resample(oldSig(5,:),P,Q);
-       newSig6 = resample(oldSig(6,:),P,Q);
-       sig(1,:) = newSig;
-       sig(2,:) = newSig2;
-       sig(3,:) = newSig3;
-       sig(4,:) = newSig4;
-       sig(5,:) = newSig5;
-       sig(6,:) = newSig6;
-       
-       % Save
-       save([path,'Training_data','/',file],'sig')
+              
+       numberOfParts = floor((size(sig,2))/(winLen-step));
+       segmentsNum = segmentsNum + numberOfParts;
+       newSig=zeros(size(sig,1),winLen,numberOfParts);      
+      
+
+        for w=1:numberOfParts-1
+
+            if(w>1)
+                newSig(:,:,w)=(sig(:,floor((w-1)*step):floor((w-1)*step)+winLen-1));
+                [qrs_amp_raw,qrs_i_raw,delay]=pan_tompkin(newSig(1,:,w),Fs,0);
+                ecgHR = length(qrs_i_raw);
+                 newSig(1,:,w)= ones(size(newSig(1,:,w)))*ecgHR;
+                %B = reshape(sig(:,(w-1)*winLen+1:(w-1)*winLen+winLen),[size(newSig,2),size(newSig,3)])
+            else
+                newSig(:,:,w)=(sig(:,floor((w-1)*step)+1:floor((w-1)*step)+winLen));                
+            end
+        end
+        
+        WindowSigTrain{contSigns} = newSig;
+        contSigns = contSigns+1;       
+
    end
     
 end
 
 
+%% test Set
+% Make labels sequences
+pathLabels = '/home/lex/Desktop/PPG_DATASET/1_IEEE_Signal_Processing_cup_2015/TestData/TrueBPM/'
+files = dir([pathLabels,'/*.mat']);
+segmentsNumTest = 0;
 files = dir([path,'TestData','/*.mat']);
-
+contSigns = 1;
 for i=1:length(files)
     
    file = files(i).name;
    if(file(1,end-4)~='e')
        load([path,'TestData','/',file])
-       oldSig = sig;
-       clear sig
-       newSig = resample(oldSig(1,:),P,Q);
-       sig = zeros(5,size(newSig,2));
-       newSig2 = resample(oldSig(2,:),P,Q);
-       newSig3 = resample(oldSig(3,:),P,Q);
-       newSig4 = resample(oldSig(4,:),P,Q);
-       newSig5 = resample(oldSig(5,:),P,Q);
-       sig(1,:) = newSig;
-       sig(2,:) = newSig2;
-       sig(3,:) = newSig3;
-       sig(4,:) = newSig4;
-       sig(5,:) = newSig5;
        
-       % Save
-       save([path,'TestData','/',file],'sig')
+       % Load labels       
+       load([pathLabels,'True',file(5:end)])
+       % Extend labels
+       labelsSig = zeros(size(sig,2),1);
+       stepLabel = 51*2;
+       for l=1:size(BPM0,1)
+           try
+               labelsSig((l-1)*stepLabel+1:(l-1)*stepLabel+stepLabel) = ...
+                   ones(size(labelsSig((l-1)*stepLabel+1:(l-1)*stepLabel+stepLabel)))*BPM0(l); 
+           catch
+               labelsSig((l-1)*stepLabel+1:end) = ...
+                   ones(size(labelsSig((l-1)*stepLabel+1:end)))*BPM0(l);                
+           end
+           
+       end 
+       
+        
+       
+       numberOfParts = floor((size(sig,2))/(winLen-step));
+       segmentsNumTest = segmentsNumTest + numberOfParts;
+       newSig=zeros(size(sig,1)+1,winLen,numberOfParts);
+      
+        for w=1:numberOfParts-1
+
+            if(w>1)
+                newSig(2:end,:,w)=(sig(:,floor((w-1)*step):floor((w-1)*step)+winLen-1));
+                newSig(1,:,w)=(labelsSig(floor((w-1)*step):floor((w-1)*step)+winLen-1));
+                %B = reshape(sig(:,(w-1)*winLen+1:(w-1)*winLen+winLen),[size(newSig,2),size(newSig,3)])
+            else
+                newSig(2:end,:,w)=(sig(:,floor((w-1)*step)+1:floor((w-1)*step)+winLen)); 
+                newSig(1,:,w)=(labelsSig(floor((w-1)*step)+1:floor((w-1)*step)+winLen));
+            end
+        end
+        
+        WindowSigTest{contSigns} = newSig;
+        contSigns = contSigns+1;       
+
    end
     
+end
+
+
+%% Cells to matrix and padding
+trainMat = zeros(6,winLen,segmentsNum);
+testMat = zeros(6,winLen,segmentsNumTest);
+antVal = 1;
+
+for i=1:size(WindowSigTrain,2)
+    
+    tempMat = WindowSigTrain{i};
+    trainMat(:,:,antVal:antVal+size(tempMat,3)-1) = tempMat;
+    antVal = antVal+size(tempMat,3);
+    
+end
+
+antVal = 1;
+for i=1:size(WindowSigTest,2)
+    
+    tempMat = WindowSigTest{i};
+    trainMat(:,:,antVal:antVal+size(tempMat,3)-1) = tempMat;
+    antVal = antVal+size(tempMat,3);    
+
 end
